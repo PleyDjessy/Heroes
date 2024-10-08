715dfa26abaeb5f18e4ccb5f0f7edf229230d4d0
PleyDjessy <pleydjessy.09@gmail.com>
2024-10-08 19:53:34 +0200

    Начало написания класса MyHero, наследника класса Hero. Пишутся методы add_skill и get_character_class

--- heroClasses/heroesClasses.py
+++ heroClasses/heroesClasses.py
@@ -36,22 +36,22 @@ class Hero:
 
     def get_new_level(self):
         if self.get_exp() >= 1000:
-            self.level = 3
+            self.__level = 3
             self.add_skill()
 
         elif self.get_exp() >= 500:
-            self.level = 2
+            self.__level = 2
             self.add_skill()
 
         elif self.get_exp() >= 200:
-            self.level = 1
+            self.__level = 1
             self.add_skill()
         else:
             self.__level = 0
-        return f"Герой {self.name}, теперь {self.level} уровня, навыки: {', '.join(self.get_my_hero_skills())}"
+        return f"Герой {self.get_name()}, теперь {self.get_level()} уровня, навыки: {', '.join(self.get_my_hero_skills())}"
 
     def add_exp(self, exp):
-        self.exp += exp
+        self.__exp += exp
         new_level = self.get_new_level()
         return new_level
 
@@ -60,4 +60,19 @@ class Hero:
 
 
 class MyHero(Hero):
-    pass
+    def __init__(self, name, character_class):
+        super().__init__(name)
+        self.__character_class = character_class
+        self.skill_list = super().get_skills(character_class)
+
+    def get_character_class(self):
+        return ""
+
+    def add_skill(self):
+        if super().get_level() > 0:
+            for skill_give in range(super().get_level()):
+                while True:
+                    print(super().get_skills(self.get_character_class()))
+
+        else:
+            return "Ошибка: уровень вашего героя равен 0"
