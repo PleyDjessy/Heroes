b617e51fc68100f05bbfe98def8bfeed60189f48
PleyDjessy <pleydjessy.09@gmail.com>
2024-10-08 17:49:10 +0200

    в классе Hero: добавлены геттеры и защита полей, обновлён метод get_new_level

--- heroClasses/heroesClasses.py
+++ heroClasses/heroesClasses.py
@@ -1,15 +1,27 @@
 class Hero:
     """Добавлен базовый класс Hero"""
-    __mage_skills = ["огненный шар", "ледяная стрела", "удар молнии"]
-    __warrior_skills = ["удар в прыжке", "вой", "берсерк"]
-    __ranger_skills = ["быстрая стрельба", "двойной выстрел", "скрытность"]
 
     def __init__(self, name):
         """Написан конструктор для класса"""
-        self.name = name
-        self.my_hero_skills = []
-        self.level = 0
-        self.exp = 0
+        self.__name = name
+        self.__my_hero_skills = []
+        self.__level = 0
+        self.__exp = 0
+        self.__mage_skills = ["огненный шар", "ледяная стрела", "удар молнии"]
+        self.__warrior_skills = ["удар в прыжке", "вой", "берсерк"]
+        self.__ranger_skills = ["быстрая стрельба", "двойной выстрел", "скрытность"]
+
+    def get_name(self):
+        return self.__name
+
+    def get_my_hero_skills(self):
+        return self.__my_hero_skills
+
+    def get_level(self):
+        return self.__level
+
+    def get_exp(self):
+        return self.__exp
 
     def get_skills(self, character_class):
         """Добавлен геттер для навыков, результат зависит от выбранного класса"""
@@ -20,21 +32,23 @@ class Hero:
         elif character_class == 'рейнджер':
             return self.__ranger_skills
         else:
-            exit("Ошибка перезапустите программу!")
+            exit("Ошибка: перезапустите программу!")
 
     def get_new_level(self):
-        if self.exp >= 1000:
+        if self.get_exp() >= 1000:
             self.level = 3
             self.add_skill()
-        elif self.exp >= 500:
+
+        elif self.get_exp() >= 500:
             self.level = 2
             self.add_skill()
-        elif self.exp >= 200:
+
+        elif self.get_exp() >= 200:
             self.level = 1
             self.add_skill()
         else:
-            self.level = 0
-        return f"Герой {self.name}, теперь {self.level} уровня, навыки: {', '.join(self.my_hero_skills)}"
+            self.__level = 0
+        return f"Герой {self.name}, теперь {self.level} уровня, навыки: {', '.join(self.get_my_hero_skills())}"
 
     def add_exp(self, exp):
         self.exp += exp
